b9376806f98c4180631a376c2e5d57325d656196
Vishvam <s.vishvam1025@gmail.com>
2022-08-21 17:07:37 +0530

    Created Base Remark Website

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,86 +1,117 @@
 <template>
      <div class="home">
         <nav>
-            <router-link to="/login" style="color: white; position: absolute; right: 24rem;">Login</router-link>
-            <router-link to="/signup" style="color: white; position: absolute; right: 8rem;">Sign Up</router-link>
+            <router-link to="/login" style="color: black; position: absolute; right: 24rem;">Login</router-link>
+            <router-link to="/signup" style="color: black; position: absolute; right: 8rem;">Sign Up</router-link>
         </nav>
         <div class="row">
             <section class="p-header">
-                <!-- <h4 class="header-text">Welcome to</h4> -->
+                <h4 class="header-text">Annotate any website with</h4>
+                <span class="remark_text">REMARK</span>
                 <div class="row" style="margin: 8rem 0rem 2rem 0rem">
                     <div class="col-6">
-                        <div class="customButton signUpButton">
-                          <router-link to="/login" style="color: black;">Login</router-link>
-                        </div>
+                        <router-link to="/login" style="color: black;">
+                            <div class="customButton loginButton">
+                                Login
+                            </div>
+                        </router-link>
                     </div>
                     <div class="col-6">
-                        <div class="customButton loginButton">
-                          <router-link to="/signup" style="color: white;">Sign Up</router-link>
-                        </div>
+                        <router-link to="/signup" style="color: white;">
+                            <div class="customButton signUpButton">
+                                Sign Up
+                            </div>
+                        </router-link>
                     </div>
                 </div>
-                <p class="header-details">Remark is a . . .</p>
+                <p class="header-details">
+                    Scroll down to know more
+                </p>
             </section>
         </div>
         <div class="row">
-            <section class="features">
-                <div class="row">
-                    <h1 style="font-weight: bold; font-size: 8rem; margin: 0rem 0rem 4rem 36rem;">Features</h1>
-                </div>
-                <div class="row features-row" style="background: linear-gradient(to right, rgb(79, 172, 254), rgb(0, 242, 254));">
-                    <div class="col-8">
+            <section class="remark_section" style="margin: 18rem 0rem 8rem 0rem;">
+                <div class="container">
+                    <div class="row">
+                        <h1 style="font-weight: bold; font-size: 8rem; margin: 0rem 0rem 2rem 0rem">What is it ?</h1>
+                        <p class="text" style="width: 40%">Remark is a lightweight, easy-to-use and hihgly customizable JavaScript library for annotating websites. The logic is implemented in pure JS and markup and styling is taken care by HTML5 and CSS3.</p>
                     </div>
-                    <div class="col-4">
-                        <div style="margin: 0rem 0rem 0rem -5rem;">
-                            <h2 class="features-heading">Create a Deck</h2>
-                            <p class="features-details">With just one click of button, you can create a custom deck !</p>
-                            <p class="features-details">This can have a custom name and can hold unlimited questions</p>
+
+                </div>
+            </section>
+        </div>
+        <div class="row">
+            <section class="remark_section" style="margin: 18rem 0rem 12rem 0rem;">
+                <div class="container">
+                    <div class="row">
+                        <h1 style="font-weight: bold; font-size: 8rem; margin: 0rem 0rem 2rem 0rem">Installation</h1>
+                        <div class="col-3">
+                            <div class="card">
+                                <h4 class="card_header">1</h4>
+                                <p class="card_text">Create an admin account.</p>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="row features-row" style="background: linear-gradient(to right, rgb(9, 150, 206), rgb(56, 249, 215));">
-                    <div class="col-4">
-                        <div style="width: 20rem;">
-                            <h2 class="features-heading">Play</h2>
-                            <p class="features-details">In every turn, you get a random flashcard from the deck. Try to recollect the correct answers ! </p>
+                        <div class="col-3 gx-5">
+                            <div class="card">
+                                <h4 class="card_header">2</h4>
+                                <p class="card_text">Register your website.</p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-8">
-                    </div>
-                </div>
-                <div class="row features-row" style="background: linear-gradient(to right, rgb(42, 245, 152), rgb(13, 173, 101));">
-                    <div class="col-8">
-                    </div>
-                    <div class="col-4">
-                        <div style="width: 20rem;">
-                            <h2 class="features-heading" style="margin: 0rem 0rem 1rem -6rem;">Performance Analysis</h2>
-                            <p class="features-details" style="margin: 2rem 1rem 1rem -5.6rem;">Get a report of your overall scores. This includes average and the standard deviation of it. You also get a recommendation for revising certain decks.</p>
+                        <div class="col-3 gx-5">
+                            <div class="card">
+                                <h4 class="card_header">3</h4>
+                                <p class="card_text">Download the script file along with the credentials.</p>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="row features-row" style="background: linear-gradient(to right, rgb(15, 216, 80), rgb(249, 240, 71)); ">
-                    <div class="col-4">
-                        <div style="width: 20rem; margin:-1.6rem 0rem 0rem 6rem">
-                            <h2 class="features-heading" style="margin: 0rem 0rem 1rem -6rem;">Deck-Wise Analysis</h2>
-                            <p class="features-details" style="margin: 2rem 1rem 1rem -5.6rem;">Get a graphical view of your deck-wise perfomance and focus on improving on the harder decks</p>
-                        </div>
-                    </div>
-                    <div class="col-8">
-                    </div>
-                </div>
-                <div class="row features-row" style="background: linear-gradient(to right, rgb(246, 211, 101), rgb(253, 160, 133));">
-                    <div class="col-8">
-                    </div>
-                    <div class="col-4">
-                        <div style="width: 20rem; margin: -2rem 0rem 0rem 0rem;">
-                            <h2 class="features-heading" style="margin: 0rem 0rem 2rem -7rem; transform: translateY(0.3rem);">Deck Management</h2>
-                            <p class="features-details" style="margin: 1rem 1rem 1rem -6.6rem;">You have full freedom ! You can create, update and delete cards and decks as and when you require.</p>
+                        <div class="col-3 gx-5">
+                            <div class="card">
+                                <h4 class="card_header">4</h4>
+                                <p class="card_text">Include the script file in your HTML file</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </section>
         </div>
         <div class="row">
+            <section class="remark_section">
+                <div class="container">
+                    <div class="row">
+                        <h1 style="font-weight: bold; font-size: 8rem; margin: 0rem 0rem 12rem 0rem">Usage</h1>
+                        <div class="col-4">
+                            <div class="card usage_card">
+                                <h4 class="card_header">1</h4>
+                                <p class="card_text">Create a remark_config.js file and place the credentials in the following format :
+                                    <br><br>
+                                    <code>
+                                        const remark_config = JSON.stringify({
+                                            REMARK_API_KEY: "yourkey",
+                                            REMARK_WEBSITE_ID: "yourwebsiteid"  
+                                        })
+                                    </code>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-4 gx-5">
+                            <div class="card usage_card">
+                                <h4 class="card_header">2</h4>
+                                <p class="card_text">
+                                    That's basically it. Click on login and login as an admin to start the annotation process. To create an annotation for an element, hover over the element, <b>Right Click</b> on it and select <b>Create Annotation</b> option from the custom contextmenu.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-4 gx-5">
+                            <div class="card usage_card">
+                                <h4 class="card_header">3</h4>
+                                <p class="card_text">To create a comment, open the annotation by choosing the <b>Open Annotation</b> option from the contextmenu, then just start typing on the text input. Everything else would be pretty intuitive but if you want to know more, please visit the <a href="https://github.com/Vishvam10/ReMark-Client" class="doc_link">documentation</a></p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- <div class="row">
             <section class="try-now">
                 <div class="d-flex flex-column justify-content-center align-items-center">
                     <h1 class="heading create">Try Now !</h1>
@@ -91,12 +122,72 @@
             </section>
         </div>
         <div id="home-footer">
-            Developed with ❤ by <span class="fw-bold" style="margin: 1rem 0rem 1rem 0.6rem;">Vishvam S</span>
-        </div>
+            Developed with ❤ by <span class="fw-bold" style="margin: 1rem 0rem 1rem 0.6rem;">
+                <a href="https://github.com/Vishvam10" style="color: white; font-size: 2rem;">Vishvam S</a>
+            </span>
+        </div> -->
     </div>
 </template>
 
-<style>
+<style scoped>
+    a {
+        color: black;
+        transition: all 0.2s ease-in-out;
+    } 
+    a:hover {
+        transform: scale(1.05);
+    }
+    a:active {
+        transform: scale(1.0);
+    }
+    h1,h2,h3,h4,h5,h6 {
+        color: black;
+    }
+    ion-icon {
+        font-size: 4rem;
+        color: rgb(128, 128, 128);
+    }
+    .doc_link {
+        background: linear-gradient(to right, rgb(148, 0, 250), rgb(255, 2, 26));
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;   
+    }
+    .card {
+        width: auto;
+        height: 16rem;
+        background: white;
+        box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+        border-radius: 2rem;
+        padding: 3rem;
+        transition: all 0.2s ease-in-out;
+    }
+    .card:hover {
+        box-shadow: rgba(0, 0, 0, 0.1) 1px 2px 24px 0px, rgba(0, 0, 0, 0.02) 0px 0px 30px 1px;
+    }
+    .card:active {
+        box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+    }
+    .card_header {
+        font-size: 2.4rem;
+        font-weight: 700;
+        margin: 0rem 0rem 2rem 0rem;
+    }
+    .card_text {
+        font-size: 1.6rem;
+        color: #8f8f8f;
+    }
+    .usage_card {
+        height: 34rem;
+    }
+    .remark_text {
+        font-size: 7rem;
+        font-weight: 700;
+        background: linear-gradient(to right, rgb(148, 0, 250), rgb(255, 2, 26));
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
     .logo {
         width: 10rem; 
         height: 2rem;
@@ -109,17 +200,21 @@
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        background: black;
         width: 100vw;
     }
     .p-header {
         position: relative;
         top: 12rem;
-        width: 50rem;
+        width: 60rem;
+        height: 51rem;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
+    }
+    .remark_section {
+
+        padding: 4rem;
     }
     .header-text {
         font-size: 4rem;
@@ -145,16 +240,9 @@
         -webkit-text-fill-color: transparent;
         margin: 0rem 8rem 1rem 0rem;
     }
-    .remember {
-        background: linear-gradient(to right, rgb(11, 163, 96), rgb(60, 186, 146));
-        background-clip: text;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        margin: 0rem 0rem 0rem 3rem;
-    }
     .header-details {
         margin: 4rem 0rem 0rem 0rem;
-        font-size: 2rem;
+        font-size: 1.6rem;
         width: 72rem;
         color: gray;
         text-align: center;
@@ -169,6 +257,7 @@
         font-size: 1.6rem;
         transition: all 0.1s cubic-bezier(0.445, 0.05, 0.55, 0.95);
         margin: 0rem 6rem 0rem 0rem;
+        cursor: pointer;
     }
     .customButton:hover {
         transform: scale(1.05);
@@ -177,40 +266,15 @@
         transform: scale(1.0);
     }
     .signUpButton {
-        background-color: white;
-        color: black;
-    }
-    .signUpButton:hover {
-        background-color: white;
-        color: black;
+        background-color: black;
     }
     .loginButton {
-        border: 0.5px solid white;
-        color: white;
+        border: 0.5px solid black;
+        color: black;
     }
-    .loginButton:hover {
-        border: 0.5px solid white;
-        color: white;
-    }
-    .features {
-        position: relative;
-        top: 26rem;
-        left: -2%;
-    }
-    .features-row {
-        margin: 4rem 0rem 8rem 0rem; 
-        padding: 4rem;
-        border-radius: 2rem;
-        box-shadow: rgba(75, 77, 80, .2) 0px 8px 24px;
-    }
-    .features-heading {
-        font-size: 6.4rem;
-        font-weight: bold;
-        margin: 0rem 0rem 2rem 0rem;
-    }
-    .features-details {
-        font-size: 2rem;
-        color: black
+    .text {
+        font-size: 1.6rem;
+        color: #7d7d7d;
     }
     .try-now {
         position: relative;
@@ -238,7 +302,7 @@
         justify-content: center;
         align-items: center;
         color: white;
-        background: rgb(36, 36, 36);
+        background: #101010;
         font-size: 2rem;
     }
     #footer {
